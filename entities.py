2bf290a19efa3e0f327955330491f434132b7c18
William Holmkvist <williamholmkvist@Williams-MacBook-Pro.local>
2021-02-23 14:17:14 +0100

    scroll and animation

--- entities.py
+++ entities.py
@@ -14,12 +14,20 @@ class Block(pygame.sprite.Sprite):
 
 class Player(pygame.sprite.Sprite):#mario
 
-    stand_right = pygame.image.load("sprites/stand_right.gif")
-    stand_left = pygame.image.load("sprites/stand_left.gif")
+    images = {0: pygame.image.load("sprites/stand_right.gif"),
+            1: pygame.image.load("sprites/stand_left.gif"),
+            2: pygame.image.load("sprites/run_right1.gif"),
+            3: pygame.image.load("sprites/run_right2.gif"),
+            4: pygame.image.load("sprites/run_right3.gif"),
+            5: pygame.image.load("sprites/run_left1.gif"),
+            6: pygame.image.load("sprites/run_left2.gif"),
+            7: pygame.image.load("sprites/run_left3.gif"),
+            }
 
     def __init__(self,x_pos,y_pos):
         super().__init__()
-        self.image = self.stand_right
+        self.image = self.images[0]
+        self.dir = 0 # 0 = right, 1 = left
         self.rect = self.image.get_rect()
         self.rect.topleft = [x_pos,y_pos]
         self.hitbox = self.rect
@@ -33,10 +41,14 @@ class Player(pygame.sprite.Sprite):#mario
         # 1 = stand left
         # 2 - 4 = running right
         # 5 - 7 = running left
+        if(val in range(2,4)):
+            self.dir = 0
+        elif(val in range(5,7)):
+            self.dir = 1
         if(val == 0):
-            self.image = self.stand_right
-        elif(val == 1):
-            self.image = self.stand_left
+            self.image = self.images[self.dir]
+        else:
+            self.image = self.images[val]
 
 class Enemy(pygame.sprite.Sprite):#mario
     def __init__(self,x_pos,y_pos):
--- main.py
+++ main.py
@@ -3,11 +3,19 @@ from pygame import mixer#sounds
 from pygame.locals import *
 
 air_timer = 0
+run_timer = 0
 moving_right = False
 moving_left = False
 jump_trigger = False
 on_ground = False
+horizontal_momentum = 0
 vertical_momentum = 0
+true_scroll = [0,0]
+scroll = [0,0]
+
+mario_bros = pygame.sprite.Group()
+blocks = pygame.sprite.Group()
+enemies = pygame.sprite.Group()
 
 #def check_block_col(player,block)
 
@@ -17,6 +25,7 @@ class GUI():
         self.click=click
         self.display_height = 176
         self.display_width = 320
+        self.dead = False
 
         self.scale = 3
         self.WINDOW_SIZE = (self.display_width*self.scale,self.display_height*self.scale)
@@ -52,6 +61,18 @@ class GUI():
                     self.click=False
             game.screen.blit(start_surface,start_rect)
             pygame.display.update()
+
+    def game_over_screen(self):
+        # this shit is crashing the program
+        while self.dead:
+            game.screen.fill((0,0,0))
+            go_surface=game_font.render("Game Over",True,(255,255,255))#antialias flag
+            go_rect=go_surface.get_rect(center=(200,100))#position
+            pygame.draw.rect(game.screen,(0,0,0),go_rect)
+            game.screen.blit(go_surface,go_rect)
+            pygame.display.update()
+            clock.tick(60)
+
 
 class overworld():#level selection stuff
     def __init__(self,active,click=False):
@@ -102,6 +123,26 @@ class level():#level
 class items():#shrooms
     pass
 
+def spawn():
+    pass
+
+def check_death(player):
+    death = False
+    if (player.hitbox.bottom > 192):
+        death = True
+    return death
+
+def death_animation(player):
+    vertical_momentum = -5
+    while True:
+        vertical_momentum += 0.3
+        game.display.fill((120,180,255))
+        move_player(player, blocks)
+        mario_bros.draw(game.display)
+        if(player.rect.bottom > 300):
+            break
+
+
 #rollback function to be used with spritecollideany() below
 def collided(sprite, other):
     return sprite.hitbox.colliderect(other.hitbox)
@@ -110,19 +151,47 @@ def collided(sprite, other):
 def move_player(mario, blocks):
 
     global vertical_momentum
+    global horizontal_momentum
     global air_timer
+    global run_timer
+
+    run_timer += 0.05
 
     x,y=0,0
-    if(moving_left):
-        x -= 2
     if(moving_right):
-        x += 2
+        if(horizontal_momentum < 0.7):
+            horizontal_momentum += 0.1
+        else:
+            horizontal_momentum += 0.2
+        if(horizontal_momentum > 3):
+            horizontal_momentum = 3
+        mario.set_img((int)(2 + horizontal_momentum * run_timer % 3))
+    if(moving_left):
+        if(horizontal_momentum > -0.7):
+            horizontal_momentum -= 0.1
+        else:
+            horizontal_momentum -= 0.2
+        if(horizontal_momentum < -3):
+            horizontal_momentum = -3
+        mario.set_img((int)(5 + horizontal_momentum * run_timer % 3))
+    if not moving_left and not moving_right:
+        if horizontal_momentum < -1:
+            horizontal_momentum += 0.2
+        elif horizontal_momentum > 1:
+            horizontal_momentum -= 0.2
+        else:
+            horizontal_momentum = 0
+            run_timer = 0
+            mario.set_img(0)
+
     vertical_momentum += 0.2
     if(vertical_momentum > 4):
         vertical_momentum = 4
+
+    x=horizontal_momentum
     y=vertical_momentum
 
-    mario.update([x,0])
+    mario.update([x-scroll[0],0])
     col_block = pygame.sprite.spritecollideany(mario,blocks,collided)
     if(col_block):
         if x > 0:
@@ -130,7 +199,7 @@ def move_player(mario, blocks):
         elif x < 0:
             mario.rect.left = col_block.hitbox.right
 
-    mario.update([0,y])
+    mario.update([0,y-scroll[1]])
     col_block = pygame.sprite.spritecollideany(mario,blocks,collided)
     if(col_block):
         if y < 0:
@@ -141,11 +210,6 @@ def move_player(mario, blocks):
             mario.rect.bottom = col_block.hitbox.top
             air_timer = 0
             vertical_momentum = 0
-
-    if(x > 0):
-        mario.set_img(0)
-    elif(x < 0):
-        mario.set_img(1)
 
     air_timer += 1
 
@@ -169,6 +233,13 @@ world=overworld(True)#flag to open after start game menu
 
 while True:#Game loop
     game.display.fill((120,180,255))
+
+    scroll[0] += mario.rect.center[0] - scroll[0] - 100
+    #scroll[1] += mario.rect.center[1] - scroll[1] - 100
+
+    blocks.update(-scroll[0],-scroll[1])
+    blocks.draw(game.display)
+
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -194,13 +265,17 @@ while True:#Game loop
 
     game.start_menu(False)
     world.world_select()
-
     move_player(mario,blocks)
-
-    blocks.draw(game.display)
     mario_bros.draw(game.display)
+
+    if(check_death(mario)):
+        break
 
 
     game.screen.blit(pygame.transform.scale(game.display,game.WINDOW_SIZE),(0,0))
     pygame.display.update()
     clock.tick(120)
+
+death_animation(mario)
+game.dead = True
+game.game_over_screen()
